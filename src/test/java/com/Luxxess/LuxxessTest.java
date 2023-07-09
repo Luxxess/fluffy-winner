c21a9ccd59cc990060ceb0812591c9580b52b92e
Luxxess <enricozenker@gmail.com>
2023-07-09 12:36:37 +0200

    --

--- src/test/java/com/Luxxess/LuxxessTest.java
+++ src/test/java/com/Luxxess/LuxxessTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.Luxxess;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class LuxxessTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(LuxxessPlugin.class);
 		RuneLite.main(args);
 	}
 }
